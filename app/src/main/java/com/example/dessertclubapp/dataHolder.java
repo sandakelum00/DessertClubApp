85aa2b444a50b8707ec638bb27a7a4ca0ee9ddfd
Asus <Asus@Dekstop>
2021-05-08 12:52:18 +0530

    login

---
+++ app/src/main/java/com/example/dessertclubapp/dataHolder.java
@@ -0,0 +1,73 @@
+package com.example.dessertclubapp;
+
+public class dataHolder {
+
+    String  name,email,street,city,phone,password,confirm_password;
+
+    public dataHolder(String name, String email, String street, String city, String phone,
+                      String password, String confirm_password) {
+        this.name = name;
+        this.email = email;
+        this.street = street;
+        this.city = city;
+        this.phone = phone;
+        this.password = password;
+        this.confirm_password = confirm_password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirm_password() {
+        return confirm_password;
+    }
+
+    public void setConfirm_password(String confirm_password) {
+        this.confirm_password = confirm_password;
+    }
+}
---
+++ app/src/main/java/com/example/dessertclubapp/home_page.java
@@ -0,0 +1,44 @@
+package com.example.dessertclubapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class home_page extends AppCompatActivity {
+
+    private Button btn_logout;
+    private Button btn_menu;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        btn_logout=findViewById(R.id.button_logut);
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intoLogin= new Intent(home_page.this, login.class);
+                startActivity(intoLogin);
+            }
+        });
+
+        btn_menu=findViewById(R.id.button_menu_page);
+        btn_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Intent intent1= new Intent(home_page.this, menu.class);
+                //startActivity(intent1);
+            }
+        });
+    }
+}
