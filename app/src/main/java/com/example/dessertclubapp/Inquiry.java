abea6cf41d139b22daa732d4c13ef2022bbe22dc
sandakelum00 <randunusandakelum@gmail.com>
2021-05-08 11:51:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dessertclubapp/Inquiry.java
@@ -0,0 +1,35 @@
+package com.example.dessertclubapp;
+
+public class Inquiry {
+
+    private String Name;
+    private String Subject;
+    private String Inquiry;
+
+    public Inquiry() {
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getSubject() {
+        return Subject;
+    }
+
+    public void setSubject(String subject) {
+        Subject = subject;
+    }
+
+    public String getInquiry() {
+        return Inquiry;
+    }
+
+    public void setInquiry(String inquiry) {
+        Inquiry = inquiry;
+    }
+}
